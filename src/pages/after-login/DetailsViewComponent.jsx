49a3a378443d6bb9a9ab2ea77cd78206926dea2a
nagaraja6050266 <nagaraja6050266@gmail.com>
2025-03-07 19:20:16 +0530

    Details Components done

--- src/pages/after-login/DetailsViewComponent.jsx
+++ src/pages/after-login/DetailsViewComponent.jsx
@@ -1,42 +1,59 @@
 import { FlexBox } from "../../components/styled-components/styledComponents";
 import { Typography } from "@mui/material";
 
+const detailsJson = [
+    {
+        sectionHeader: "Personal Info",
+        values: [
+            {
+                label: "Name",
+                value: "Nagaraja",
+            },
+            {
+                label: "DOB",
+                value: "234567",
+            },
+        ],
+    },
+];
 
-const detailsJson = [{
-    sectionHeader: "Personal Info",
-    values: [
-        {
-            label: "Name",
-            value: "Nagaraja"
-        },
-        {
-            label: "DOB",
-            value: "234567"
-        }
-    ]
-}];
-
-function DetailsSegment({detail}){
-    return <FlexBox m={2} alignItems={"flex-start"} flexDirection={"column"}>
-        <Typography mt={3} variant="h4" color="primary" fontWeight="bold">
+function DetailsSegment({ detail, width }) {
+    return (
+        <FlexBox m={3} alignItems={"flex-start"} flexDirection={"column"} width={width}>
+            <Typography mt={3} variant="h4" color="primary" fontWeight="bold">
                 {detail.sectionHeader}
-        </Typography>
-        <FlexBox flexDirection={"column"}>
-            {detail.values.map((value) => {
-                return <FlexBox>
-                    <Typography fontSize={25} color={"secondary"}>{value.label}</Typography>
-                    <Typography fontSize={25}>{value.value}</Typography>
+            </Typography>
+            <FlexBox gap={2} mt={2}>
+                <FlexBox flexDirection={"column"} width={"40%"} alignItems={"flex-end"}>
+                    {detail.values.map((value) => {
+                        return (
+                            <Typography
+                                fontSize={20}
+                                fontWeight={"bold"}
+                                color={"secondary"}
+                            >
+                                {value.label}
+                            </Typography>
+                        );
+                    })}
                 </FlexBox>
-            })}
+                <FlexBox flexDirection={"column"} width={"60%"} alignItems={"flex-start"}>
+                    {detail.values.map((value) => {
+                        return (
+                            <Typography fontSize={20}>{value.value}</Typography>
+                        );
+                    })}
+                </FlexBox>
+            </FlexBox>
         </FlexBox>
-    </FlexBox>
+    );
 }
 
-export default function DetailsViewComponent({details = detailsJson}) {
+export default function DetailsViewComponent({ details = detailsJson, width = "100hw" }) {
     return (
         <>
             {details.map((detail, i) => {
-                return <DetailsSegment key={i} detail={detail} />;
+                return <DetailsSegment key={i} detail={detail} width={width} />;
             })}
         </>
     );
