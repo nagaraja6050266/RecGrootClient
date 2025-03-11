98f408eaad7d5623ee3fa9d9be5ea723c9643008
nagaraja6050266 <nagaraja6050266@gmail.com>
2025-03-11 08:15:15 +0530

    Profile changes

--- src/components/header-components/UserProfile.jsx
+++ src/components/header-components/UserProfile.jsx
@@ -1,15 +1,21 @@
 import { FlexBox } from "../common-components/styledComponents";
 import { Typography, Box } from "@mui/material";
 import userProfile from "../../assets/userProfile.png";
-function UserProfile() {
+
+const defaultProfile = {
+    name: "UserName",
+    type: "UserType",
+}
+
+function UserProfile({profile = defaultProfile}) {
     return (
         <FlexBox width={"fit-content"} height={40}gap={1}>
             <Box flexDirection="column" justifyContent="left">
                 <Typography fontWeight={600} fontSize={15}>
-                    UserName
+                    {profile.name}
                 </Typography>
                 <Typography variant="body2" fontSize={10} color="#5B738B">
-                    UserType
+                    {profile.type}
                 </Typography>
             </Box>
             <img width="auto" height="100%" src={userProfile} alt="profile" />
